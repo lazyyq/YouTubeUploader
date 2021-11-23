ff2bdf3022b203f4c6dedf2a3a24acae5d93f61b
lazyyq <kykint@naver.com>
2021-11-24 05:47:13 +0900

    Improve options for headless mode

--- main.py
+++ main.py
@@ -33,9 +33,11 @@ def main():
     elif args.browser == "chrome":
         if args.headless:
             options = webdriver.ChromeOptions()
-            options.add_argument('headless')
-            options.add_argument('window-size=1920x1080')
-            options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36')
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--start-maximized')
+            options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36')
             driver = webdriver.Chrome(options=options)
         else:
             driver = webdriver.Chrome()
